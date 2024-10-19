4d6d019d0b03cf7a7adb6177152211d9bd90fa45
Emmanuel <onyejemeemmanuel65@gmail.com>
2024-10-19 21:11:52 +0100

    removed bouncing circles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,25 @@
 
+import Activ8Advantages from "../components/Activ8Advantages";
+import AnimatedSlider from "../components/AnimatedSlider";
+import CountdownComponent from "../components/CountdownComponent";
+import HowItWorks from "../components/HowItWorks";
 import Landing from "../components/Landing";
+import NumberlessATMCards from "../components/NumberlessATMCards";
+import Plans from "../components/Plans";
+import Ready2Activ8 from "../components/Ready2Activ8";
 
 export default function Home() {
   return (
 
     <main className="min-h-screen p-0 overflow-x-hidden ">
       <Landing />
-      {/* <AnimatedSlider /> */}
-      {/* <Activ8Advantages /> */}
-      {/* <NumberlessATMCards /> */}
-      {/* <HowItWorks /> */}
-      {/* <Plans /> */}
-      {/* <Ready2Activ8 /> */}
-      {/* <CountdownComponent /> */}
+      <AnimatedSlider />
+      <Activ8Advantages />
+      <NumberlessATMCards />
+      <HowItWorks />
+      <Plans />
+      <Ready2Activ8 />
+      <CountdownComponent />
     </main>
   );
 }
